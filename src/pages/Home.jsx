997e15fdf1729551c7302b5cc7ec97f2b4959eaf
Juan Carlos Forero <juan.forero66@gmail.com>
2024-11-20 18:37:20 -0500

    front 2433452

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,17 @@ import { Link } from 'react-router-dom';
 import { FaCompass } from 'react-icons/fa';
 import CityCarousel from '../components/city/CityCarousel';
 
-// Definición del componente Home
 const Home = () => {
   return (
-    // Contenedor principal con altura mínima de pantalla completa
     <div className="min-h-screen flex flex-col">
-      {/* Sección hero con fondo animado */}
       <div className="hero-background bg-black bg-opacity-60 text-white py-20 md:py-32 lg:py-40 px-4 bg-center bg-no-repeat animate-zoom-pan-enhanced bg-[length:150%_auto]">
         <div className="container mx-auto text-center">
-          {/* Título principal con animación */}
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-6 animate-fade-in-down">
             Welcome to My Tinerary
           </h1>
-          {/* Subtítulo con animación */}
           <p className="text-lg md:text-xl lg:text-2xl mb-6 md:mb-8 lg:mb-10 animate-fade-in-up">
             Find your perfect trip, designed by insiders who know and love their cities!
           </p>
-          {/* Botón de llamada a la acción con icono y animación */}
           <Link
             to="/cities"
             className="group bg-gray-800 text-white font-bold py-3 px-6 md:py-4 md:px-8 rounded-full text-base md:text-lg lg:text-xl hover:bg-gray-900 transition duration-300 inline-flex items-center space-x-2 md:space-x-3 animate-pulse"
@@ -30,11 +24,9 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Sección de ciudades populares */}
       <div className="bg-slate-500 flex-grow">
         <div className="container mx-auto px-4 py-12">
           <h2 className="text-3xl font-bold text-white mb-8 text-center">Popular Mytineraries</h2>
-          {/* Componente de carrusel de ciudades */}
           <CityCarousel />
         </div>
       </div>
